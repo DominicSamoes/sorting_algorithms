b8d5fdd62e8645b5df3f2fc5a7c8b98c4013bfd3
DominicSamoes <dominicsamo@gmail.com>
2022-07-17 15:57:52 +0200

    Task 0-3

--- sort.h
+++ sort.h
@@ -1,25 +1,27 @@
-#ifndef SORT_H
-#define SORT_H
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _SORT_H_
+#define _SORT_H_
 
-/**
- * File: sort.h
- * Auth: Dominic Samo
- * Desc: Header file for 0x1B. C - Sorting algorithms & Big O.
- */
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-#endif
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+
+#endif /* _SORT_H_ */
